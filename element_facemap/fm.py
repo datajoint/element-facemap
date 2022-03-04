145ecf3a79717a0b1b29f1c794792f4360b8f102
tdincer <tolgadincer@gmail.com>
2022-03-04 15:14:32 -0600

    switch from - to _

--- element_facemap/fm.py
+++ element_facemap/fm.py
@@ -1,12 +1,8 @@
-import datajoint as dj
-from element_interface.utils import find_full_path, dict_to_uuid, find_root_directory
-import os
 import pathlib
 import inspect
 import importlib
-import numpy as np
-from pathlib import Path
-from datetime import datetime
+import datajoint as dj
+from element_interface.utils import find_full_path, dict_to_uuid, find_root_directory
 
 schema = dj.schema()
 _linking_module = None
@@ -63,7 +59,7 @@ def get_fm_root_data_dir() -> list:
     It is recommended that all paths in DataJoint Elements stored as relative
     paths, with respect to some user-configured "root" director(y/ies). The
     root(s) may vary between data modalities and user machines
-    get_mp_root_data_dir() -> list
+    get_fm_root_data_dir() -> list
         This user-provided function retrieves the possible root data
         director(y/ies) containing continuous face data for all subjects
         and sessions (e.g. acquired video raw files)
@@ -71,7 +67,7 @@ def get_fm_root_data_dir() -> list:
          or list of strings for possible root data directories
     """
     root_directories = _linking_module.get_fm_root_data_dir()
-    if isinstance(root_directories, (str, Path)):
+    if isinstance(root_directories, (str, pathlib.Path)):
         root_directories = [root_directories]
 
     if hasattr(_linking_module, 'get_fm_processed_data_dir'):
@@ -113,6 +109,16 @@ class VideoRecording(dj.Manual):
         -> master
         file_path: varchar(255)  # filepath of video, relative to root data directory
         """
+    
+    class Info(dj.Part):
+        definition = """
+        -> master
+        ---
+        px_height         : smallint  # height in pixels
+        px_width          : smallint  # width in pixels
+        fps               : float     # (Hz) frames per second
+        """
+
 
 @schema
 class FacemapParamSet(dj.Lookup):
@@ -153,5 +159,73 @@ class ProcessingTask(dj.Manual):
     -> VideoRecording
     -> FacemapParamSet
     ---
+    processing_output_dir: varchar(255)         #  output directory that will keep the facemap results
+    task_mode='load': enum('load', 'trigger')   # 'load': load computed analysis results, 'trigger': trigger computation
     """
 
+
+@schema
+class Processing(dj.Computed):
+    definition = """  # Processing Procedure
+    -> ProcessingTask
+    ---
+    processing_time     : datetime  # time of generation of the facemap results
+    package_version=''  : varchar(16)
+    """
+    
+    
+    # Run processing only on Scan with ScanInfo inserted
+    @property
+    def key_source(self):
+        return ProcessingTask & VideoRecording.File
+
+    def make(self, key):
+        task_mode = (ProcessingTask & key).fetch1('task_mode')
+
+        output_dir = (ProcessingTask & key).fetch1('processing_output_dir')
+        if not output_dir:
+            output_dir = ProcessingTask.infer_output_dir(key, relative=True, mkdir=True)
+            # update processing_output_dir
+            ProcessingTask.update1({**key, 'processing_output_dir': output_dir.as_posix()})
+
+        if task_mode == 'load':
+            method, imaging_dataset = get_loader_result(key, ProcessingTask)
+            if method == 'suite2p':
+                if (scan.ScanInfo & key).fetch1('nrois') > 0:
+                    raise NotImplementedError(f'Suite2p ingestion error - Unable to handle'
+                                              f' ScanImage multi-ROI scanning mode yet')
+                suite2p_dataset = imaging_dataset
+                key = {**key, 'processing_time': suite2p_dataset.creation_time}
+            elif method == 'caiman':
+                caiman_dataset = imaging_dataset
+                key = {**key, 'processing_time': caiman_dataset.creation_time}
+            else:
+                raise NotImplementedError('Unknown method: {}'.format(method))
+
+
+
+# ---------------- HELPER FUNCTIONS ----------------
+
+
+_table_attribute_mapper = {'ProcessingTask': 'processing_output_dir'}
+
+
+def get_loader_result(key, table):
+    """
+    Retrieve the loaded processed imaging results from the fissa loader
+        :param key: the `key` to one entry of ProcessingTask
+        :param table: the class defining the table to retrieve
+         the loaded results from (e.g. ProcessingTask)
+        :return: a loader object of the loaded results
+         (e.g. suite2p.Suite2p, caiman.CaImAn, etc.)
+    """
+    output_dir = (FacemapParamSet * table & key).fetch1(
+        'processing_method', _table_attribute_mapper[table.__name__])
+
+    output_path = find_full_path(get_fm_root_data_dir(), output_dir)
+
+    from element_interface import fissa_loader
+    
+    loaded_dataset = fissa_loader(output_path)
+
+    return loaded_dataset
