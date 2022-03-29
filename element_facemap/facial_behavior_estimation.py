4533602de2e1776949df26b3b2ce670cf2345c9f
tdincer <tolgadincer@gmail.com>
2022-03-28 20:32:56 -0500

    pep8 integrity

--- element_facemap/facial_behavior_estimation.py
+++ element_facemap/facial_behavior_estimation.py
@@ -12,32 +12,33 @@ schema = dj.schema()
 
 _linking_module = None
 
+
 def activate(facemap_schema_name, *, create_schema=True, create_tables=True,
              linking_module=None):
     """
     activate(schema_name, *, create_schema=True, create_tables=True,
              linking_module=None)
-        :param schema_name: schema name on the database server to activate the
-                            `facemap` element
-        :param create_schema: when True (default), create schema in the database if it
-                              does not yet exist.
-        :param create_tables: when True (default), create schema in the database if it
-                              does not yet exist.
-        :param linking_module: a module (or name) containing the required dependencies
-                               to activate the `session` element:
-            Upstream tables:
-                + Session: parent table to VideoRecording, identifying a recording session
-                + Device: parent table to VideoRecording, identifying video recording device
-            Functions:
-                + get_facemap_root_data_dir() -> list
-                    Retrieve the root data director(y/ies) with face
-                    recordings for all subject/sessions.
-                    :return: a string for full path to the root data directory
-                + get_facemap_processed_data_dir(session_key: dict) -> str
-                    Optional function to retrive the desired output directory
-                    for Facemap files for a given session. If unspecified,
-                    output stored in the session video folder, per Facemap default
-                    :return: a string for the absolute path of output directory
+    :param schema_name: schema name on the database server to activate the
+                        `facemap` element
+    :param create_schema: when True (default), create schema in the database if it
+                            does not yet exist.
+    :param create_tables: when True (default), create schema in the database if it
+                            does not yet exist.
+    :param linking_module: a module (or name) containing the required dependencies
+                            to activate the `session` element:
+        Upstream tables:
+            + Session: parent table to VideoRecording, identifying a recording session
+            + Device: parent table to VideoRecording, identifying video recording device
+        Functions:
+            + get_facemap_root_data_dir() -> list
+                Retrieve the root data director(y/ies) with face
+                recordings for all subject/sessions.
+                :return: a string for full path to the root data directory
+            + get_facemap_processed_data_dir(session_key: dict) -> str
+                Optional function to retrive the desired output directory
+                for Facemap files for a given session. If unspecified,
+                output stored in the session video folder, per Facemap default
+                :return: a string for the absolute path of output directory
     """
 
     if isinstance(linking_module, str):
@@ -97,7 +98,7 @@ def get_facemap_processed_data_dir() -> str:
 
 def get_facemap_video_files(video_key: dict) -> str:
     """
-    Retrieve the list of video files (e.g. *.avi) associated with a given video recording
+    Retrieve the list of video files associated with a given video recording
     :param video_key: key of a video recording
     :return: list of Video files' full file-paths
     """
@@ -120,9 +121,9 @@ class VideoRecording(dj.Manual):
     class File(dj.Part):
         definition = """
         -> master
-        file_id         : smallint
+        file_id     : smallint
         ---
-        file_path       : varchar(255)  # filepath of video, relative to root data directory
+        file_path   : varchar(255)  # filepath of video, relative to root directory
         """
 
 
@@ -133,10 +134,10 @@ class RecordingInfo(dj.Imported):
     ---
     px_height                 : smallint  # height in pixels
     px_width                  : smallint  # width in pixels
-    nframes                   : smallint  # number of frames 
-    fps = NULL                : int     # (Hz) frames per second
-    recording_duration = NULL : float     # video duration in seconds
-    recording_time = NULL     : datetime  # Time at the beginning of the recording with respect to the session
+    nframes                   : smallint  # number of frames
+    fps = NULL                : int       # frames per second in Hz
+    duration = NULL           : float     # video duration in seconds
+    recording_time = NULL     : datetime  # time at the beginning of the recording
     """
 
     @property
@@ -150,10 +151,14 @@ class RecordingInfo(dj.Imported):
         px_height, px_width, fps = None, None, None
 
         for file_path in file_paths:
-            file_path = (find_full_path(get_facemap_root_data_dir(), file_path)).as_posix()
+            file_path = (find_full_path(
+                get_facemap_root_data_dir(), file_path)
+                ).as_posix()
 
             cap = cv2.VideoCapture(file_path)
-            info = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FPS))
+            info = (int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),
+                    int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                    int(cap.get(cv2.CAP_PROP_FPS)))
             if px_height is not None:
                 assert (px_height, px_width, fps) == info
             px_height, px_width, fps = info
@@ -165,30 +170,30 @@ class RecordingInfo(dj.Imported):
             'px_height': px_height,
             'px_width': px_width,
             'nframes': nframes,
-            'fps': fps,                   # Assuming that this keyword is set correctly but it's usually not.
-            'recording_duration': nframes / fps,    # TODO: Something to think about given the fact above
+            'fps': fps,                 # usually user-defined and wrong
+            'duration': nframes / fps,  # see caution above
         })
-    
+
 
 @schema
 class FacemapTask(dj.Manual):
-    definition = """  # Configuration for a facemap analysis task on a particular VideoRecording
-    # Parameters used to run the Facemap on each Recording
+    definition = """
+    # Configuration for a facemap analysis task on a particular VideoRecording
     -> VideoRecording
     facemap_task_id             : smallint
     ---
-    processing_output_dir=NULL  : varchar(255)              # output directory that will keep the facemap results
-    task_mode='load'            : enum('load', 'trigger')   # 'load': load computed analysis results, 'trigger': trigger computation
-    facemap_params              : longblob                  # dictionary with region of interest specification, and other params if applicable
-    do_mot_svd=1                : bool                      # whether to decompose the motion images
-    do_mov_svd=0                : bool                      # whether to decompose the movie images
+    processing_output_dir=NULL  : varchar(255)
+    task_mode='load'            : enum('load', 'trigger')
+    facemap_params              : longblob              # content of _proc.npy (dict)
+    do_mot_svd=1                : bool
+    do_mov_svd=0                : bool
     task_description=''         : varchar(128)
     """
 
     def infer_output_dir(self, key, relative=True, mkdir=True):
-        video_file = (VideoRecording.File & key).fetch('file_path', limit=1)[0]  # Take 1 video file
-        video_dir = find_full_path(get_facemap_root_data_dir(), video_file).parent  # find video file's full path
-        root_dir = find_root_directory(get_facemap_root_data_dir(), video_dir)  # find the video file's root directory
+        video_file = (VideoRecording.File & key).fetch('file_path', limit=1)[0]
+        video_dir = find_full_path(get_facemap_root_data_dir(), video_file).parent
+        root_dir = find_root_directory(get_facemap_root_data_dir(), video_dir)
 
         paramset_key = (FacemapTask & key).fetch1('facemap_task_id')
         processed_dir = Path(get_facemap_processed_data_dir())
@@ -196,7 +201,7 @@ class FacemapTask(dj.Manual):
 
         if mkdir:
             output_dir.mkdir(parents=True, exist_ok=True)
-        
+
         return output_dir.relative_to(processed_dir) if relative else output_dir
 
 
@@ -206,11 +211,11 @@ class FacemapProcessing(dj.Computed):
     # Processing Procedure
     -> FacemapTask
     ---
-    processing_time             : datetime  # time of generation of the facemap results
-    package_version=''          : varchar(16)
+    processing_time     : datetime  # time of generation of the facemap results
+    package_version=''  : varchar(16)
     """
-    
-    # Processing only the VideoRecordings that have their Info inserted.
+
+    # Process only the VideoRecordings that have their Info inserted.
     @property
     def key_source(self):
         return FacemapTask & VideoRecording.File
@@ -227,13 +232,17 @@ class FacemapProcessing(dj.Computed):
 
         if task_mode == 'trigger':
             from facemap.process import run as facemap_run
-            facemap_params, motSVD, movSVD = (FacemapTask & key).fetch1('facemap_params', 'do_mot_svd', 'do_mov_svd')
+            facemap_params, motSVD, movSVD = (FacemapTask & key).fetch1(
+                'facemap_params', 'do_mot_svd', 'do_mov_svd')
 
             video_files = (FacemapTask * VideoRecording.File & key).fetch('file_path')
-            video_files = [find_full_path(get_facemap_root_data_dir(), video_file).as_posix() for video_file in video_files]
+            video_files = [[find_full_path(get_facemap_root_data_dir(), video_file).as_posix() for video_file in video_files]]
 
             output_dir = find_full_path(get_facemap_root_data_dir(), output_dir)
-            facemap_run([video_files], sbin=facemap_params['sbin'], proc=facemap_params, savepath=output_dir.as_posix(), motSVD=facemap_params['motSVD'], movSVD=facemap_params['movSVD'])
+            facemap_run(
+                video_files, sbin=facemap_params['sbin'], proc=facemap_params,
+                savepath=output_dir.as_posix(), motSVD=facemap_params['motSVD'],
+                movSVD=facemap_params['movSVD'])
 
         _, creation_time = get_loader_result(key, FacemapTask)
         key = {**key, 'processing_time': creation_time}
@@ -251,56 +260,55 @@ class FacialSignal(dj.Imported):
     class Region(dj.Part):
         definition = """
         -> master
-        roi_no              : int           # Region no
+        roi_no        : int         # Region no
         ---
-        roi_name=''         : varchar(16)   # user-friendly name of the roi
-        xrange              : longblob      # 1d np.array - x pixel indices of the region
-        yrange              : longblob      # 1d np.array - y pixel indices of the region
-        xrange_bin          : longblob      # 1d np.array - binned x pixel indices of the region
-        yrange_bin          : longblob      # 1d np.array - binned y pixel indices of the region
-        motion              : longblob      # 1d np.array - absolute motion energies across time (nframes) - sum of pixels in the motion frames constructed from motSVD
+        roi_name=''   : varchar(16) # user-friendly name of the roi
+        xrange        : longblob    # 1d np.array - x pixel indices
+        yrange        : longblob    # 1d np.array - y pixel indices
+        xrange_bin    : longblob    # 1d np.array - binned x pixel indices
+        yrange_bin    : longblob    # 1d np.array - binned y pixel indices
+        motion        : longblob    # 1d np.array - absolute motion energies (nframes)
         """
-
 
     class MotionSVD(dj.Part):
         definition = """
         -> master.Region
-        component_no        : int           # principle component no
+        pc_no         : int         # principle component (PC) no
         ---
-        singular_value      : float         # singular value corresponding to the principle component
-        motmask             : longblob      # principle component - 2D motion mask (y, x)
-        projection          : longblob      # projections onto the principle component - 1D motSVD array of length: nframes
+        singular_value: float       # singular value corresponding to the PC
+        motmask       : longblob    # PC (y, x)
+        projection    : longblob    # projections onto the principle component (nframes)
         """
 
     class MovieSVD(dj.Part):
         definition = """
         -> master.Region
-        component_no        : int           # principle component no
+        pc_no         : int         # principle component (PC) no
         ---
-        singular_value      : float         # singular value corresponding to the principle component
-        movmask             : longblob      # principle component - 2D motion mask (y, x)
-        projection          : longblob      # projections onto the principle component - 1D motSVD array of length: nframes
+        singular_value: float       # singular value corresponding to the PC
+        movmask       : longblob    # PC (y, x)
+        projection    : longblob    # projections onto the principle component (nframes)
         """
 
     class Summary(dj.Part):
         definition = """
         -> master
         ---
-        sbin                : int           # spatial bin size
-        avgframe            : longblob      # 2d np.array - average binned frame
-        avgmotion           : longblob      # 2d nd.array - average binned motion frame
+        sbin          : int         # spatial bin size
+        avgframe      : longblob    # 2d np.array - average binned frame
+        avgmotion     : longblob    # 2d nd.array - average binned motion frame
         """
 
     @staticmethod
     def get_ncomponents(Svs, threshold=0.95):
-        # Calculate the number of PCA components that will make up to the first 95% variance.
+        # Calculate the number of PCA components that will make up to
+        # the first 95% variance.
         if Svs.sum() == 0.:
             return len(Svs)
         else:
             squared_Svs = Svs ** 2
             cumulative_explained_variances = np.cumsum(squared_Svs / sum(squared_Svs))
             return sum(cumulative_explained_variances < threshold)
-
 
     def make(self, key):
         dataset, _ = get_loader_result(key, FacemapTask)
@@ -328,9 +336,9 @@ class FacialSignal(dj.Imported):
                     dict(
                         key,
                         roi_no=roi_no,
-                        component_no=i,
+                        pca_no=i,
                         singular_value=dataset['motSv'][i],
-                        motmask=dataset['motMask_reshape'][roi_no+1][:,:,i],
+                        motmask=dataset['motMask_reshape'][roi_no+1][:, :, i],
                         projection=dataset['motSVD'][roi_no+1][i],
                     ))
         self.MotionSVD.insert(entry)
@@ -344,10 +352,10 @@ class FacialSignal(dj.Imported):
                     dict(
                         key,
                         roi_no=roi_no,
-                        component_no=i,
+                        pca_no=i,
                         singular_value=dataset['movSv'][i],
-                        movmask=dataset['movMask_reshape'][roi_no+1][:,:,i],
-                        projection=dataset['movSVD'][roi_no+1][i],
+                        movmask=dataset['movMask_reshape'][roi_no + 1][:, :, i],
+                        projection=dataset['movSVD'][roi_no + 1][i],
                     ))
         self.MovieSVD.insert(entry)
 
@@ -370,14 +378,14 @@ def get_loader_result(key, table):
         :param key: the `key` to one entry of FacemapTask
         :param table: the class defining the table to retrieve
          the loaded results from (e.g. FacemapTask)
-        :return: output dictionary in the _proc.npy and the creation date time 
+        :return: output dictionary in the _proc.npy and the creation date time
     """
     output_dir = (table & key).fetch1('processing_output_dir')
 
     output_path = find_full_path(get_facemap_root_data_dir(), output_dir)
     output_file = glob(output_path.as_posix() + '/*_proc.npy')[0]
-    
+
     loaded_dataset = np.load(output_file, allow_pickle=True).item()
-    creation_time = (datetime.fromtimestamp(Path(output_file).stat().st_ctime))#.isoformat()
+    creation_time = (datetime.fromtimestamp(Path(output_file).stat().st_ctime))
 
     return loaded_dataset, creation_time
