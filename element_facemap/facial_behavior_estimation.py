d215957726f843af51520b7221b48699d103b85d
Tolga Dincer <tolgadincer@gmail.com>
2022-04-20 16:26:40 -0500

    Update element_facemap/facial_behavior_estimation.py
    Co-authored-by: Dimitri Yatsenko <dimitri@datajoint.com>

--- element_facemap/facial_behavior_estimation.py
+++ element_facemap/facial_behavior_estimation.py
@@ -345,7 +345,7 @@ class FacialSignal(dj.Imported):
                             roi_no=roi_no,
                             pc_no=i,
                             singular_value=dataset["motSv"][i]
-                            if "motSV" in dataset.keys()
+                            if "motSV" in dataset
                             else None,
                             motmask=dataset["motMask_reshape"][roi_no + 1][:, :, i],
                             projection=dataset["motSVD"][roi_no + 1][i],
