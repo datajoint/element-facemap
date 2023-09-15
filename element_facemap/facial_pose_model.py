7e0e7aca9ab60de6d76b25e853078b7e7ff1dc9f
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-09-15 12:38:42 -0500

    save vids as posix for insertion into pose object

--- element_facemap/facial_pose_model.py
+++ element_facemap/facial_pose_model.py
@@ -278,9 +278,12 @@ class FacemapPoseEstimation(dj.Computed):
 
             # Instantiate Pose object, with filenames specified as video files, and bounding specified in params
             # Assumes GUI to be none as we are running CLI implementation
-            test_video_files = list(Path.cwd().glob("*.mp4").as_posix())
+            test_video_files = list(Path.cwd().glob("*.mp4"))
+            vid_files = []
+            for vid in test_video_files:
+                vid_files.append(vid.as_posix())
             pose = facemap_pose.Pose(
-                filenames=test_video_files,
+                filenames=vid_files,
                 model_name=facemap_model_path.stem,
             )
             pose.run()
