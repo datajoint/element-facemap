47285b9fef161e18932f52b0386a135dfafbc870
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-09-15 12:32:45 -0500

    save files as posix

--- element_facemap/facial_pose_model.py
+++ element_facemap/facial_pose_model.py
@@ -278,7 +278,7 @@ class FacemapPoseEstimation(dj.Computed):
 
             # Instantiate Pose object, with filenames specified as video files, and bounding specified in params
             # Assumes GUI to be none as we are running CLI implementation
-            test_video_files = list(Path.cwd().glob("*.mp4"))
+            test_video_files = list(Path.cwd().glob("*.mp4").as_posix())
             pose = facemap_pose.Pose(
                 filenames=test_video_files,
                 model_name=facemap_model_path.stem,
