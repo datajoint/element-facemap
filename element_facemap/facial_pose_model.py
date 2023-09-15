bba5f09a4dad6b7e63832f2217bcb55334195229
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-09-15 12:28:34 -0500

    add test video files for debugging

--- element_facemap/facial_pose_model.py
+++ element_facemap/facial_pose_model.py
@@ -278,9 +278,9 @@ class FacemapPoseEstimation(dj.Computed):
 
             # Instantiate Pose object, with filenames specified as video files, and bounding specified in params
             # Assumes GUI to be none as we are running CLI implementation
-
+            test_video_files = list(Path.cwd().glob("*.mp4"))
             pose = facemap_pose.Pose(
-                filenames=video_files,
+                filenames=test_video_files,
                 model_name=facemap_model_path.stem,
             )
             pose.run()
