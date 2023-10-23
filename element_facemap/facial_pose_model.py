2b5595190510c71f0d22b8b47e3bb02d5b89c501
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-10-23 17:38:08 -0500

    set bbox to null

--- element_facemap/facial_pose_model.py
+++ element_facemap/facial_pose_model.py
@@ -198,7 +198,7 @@ class FacemapPoseEstimationTask(dj.Manual):
     ---
     pose_estimation_output_dir=''   : varchar(255)  # output dir - stores results of Facemap Pose estimation analysis
     task_mode='trigger'             : enum('load', 'trigger')
-    bbox=None                       : longblob  # list containing bounding box for cropping the video [x1, x2, y1, y2]
+    bbox=null                       : longblob  # list containing bounding box for cropping the video [x1, x2, y1, y2]
     task_description=''             : varchar(128)    
     """
 
