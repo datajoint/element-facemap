a1f8d9cfbdddf0e65c2606eabe42afe2e1b8bcfd
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-10-23 14:34:51 -0500

    remove alternate cv model training

--- element_facemap/train_facial_model.py
+++ element_facemap/train_facial_model.py
@@ -371,50 +371,7 @@ class FacemapModelTraining(dj.Computed):
                                             float(training_params['learning_rate']), 
                                             int(training_params['weight_decay']),
                                             bbox=training_params['bbox'])
-        
-        # Model Training with Cross Validation
-        # from facemap.pose import model_training, datasets
-
-
-        # Split dataset into train and test splits 
-        
-        # Splitting keypoints data
-        # dsplits = utils.split_data(X,Y,tcam,tneural)
-        # (
-        #     X_train,
-        #     X_test,
-        #     Y_train,
-        #     Y_test,
-        #     itrain_sample_b,
-        #     itest_sample_b,
-        #     itrain_sample,
-        #     itest_sample,
-        #     itrain,
-        #     itest,
-        # ) = dsplits
-
-        # # Splitting frames image data
-
-        # dataset = datasets.FacemapDataset(
-        #     image_data=image_data,
-        #     keypoints_data=keypoints_data.T,
-        #     bbox=training_params['bbox'],
-        # )
-        # # Create a dataloader object for training
-        # dataloader = torch.utils.data.DataLoader(
-        #     dataset, batch_size=int(training_params['batch_size']), shuffle=True
-        # )
-        # # Use preprocessed data to train the model
-        # train_model.net = model_training.train(
-        #     dataloader,
-        #     train_model.net,
-        #     int(training_params['epochs']),
-        #     int(training_params['weight_decay']),
-        # )
-
-        # pred_keypoints, keypoints = model_training.get_test_predictions(train_model.net, dataset)
-        
-
+            
         # Save Refined Model
         model_output_path = output_dir / f'{refined_model_name}.pth'
         train_model.save_model(model_output_path)
